b1feacc61a580c956e9b871872ea3a5fe172dbd6
Joao Garduño <joao.garduno@gmail.com>
2023-12-17 16:07:36 -0600

    Estructura 1: Seleccionar Elementos del DOM - .getElementById()

---
+++ app.js
@@ -0,0 +1,18 @@
+// Seleccionando un elemento por su ID
+// -----------------------------------
+// const contenedor = document.getElementById('contenedor');
+
+// console.log(contenedor)
+// console.log(typeof contenedor.innerHTML);
+
+
+// --------
+const titulo = document.getElementById('titulo');
+
+// console.log(titulo);
+// console.log(typeof titulo);
+
+// console.log(titulo.innerText);
+// console.log(typeof titulo.innerText);
+
+console.log(titulo.tagName);
