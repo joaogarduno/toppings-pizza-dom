f8fc03de3023b0d17b8e120ef959c28c17ce62db
Joao Garduño <joao.garduno@gmail.com>
2023-12-17 17:00:17 -0600

    Estructura 4: Seleccionar Elementos del DOM - .querySelector()

--- app.js
+++ app.js
@@ -31,11 +31,44 @@
 
 // -----------------------------------
 // Seleccionando todos los elementos que tengan una etiqueta específica HTML
-const misToppings = document.getElementsByTagName('li');
+// const misToppings = document.getElementsByTagName('li');
 
-console.log(misToppings);
+// console.log(misToppings);
 // console.log(typeof misToppings);
 
-const toppingsFondoMarron = document.getElementsByClassName('fondo-marron');
+// const toppingsFondoMarron = document.getElementsByClassName('fondo-marron');
 
-console.log(toppingsFondoMarron);
+// console.log(toppingsFondoMarron);
+
+
+
+// -----------------------------------
+// Seleccionar un elemento que cumpla el criterio de un selector CSS y existen 2 metodos diferentes 
+// - .querySelector() -> El primer elemento que cumpla ese criterio
+// - .querySelectorAll() -> Nos permite seleccionar todos los que cumplen con ese criterio.
+
+// Estos métodos son útiles cuando necesitamos un proceso de selección más específico, es como cuando escribes tus selectores en CSS.
+
+// Puedes combinar distintos estados ID, Clases, activo, enfocado --> .querySelector()
+
+// const aceituna = document.querySelector('#aceitunas');
+// console.log(aceituna);
+
+
+// const aceituna = document.querySelector('.topping');
+// console.log(aceituna)
+// console.log(typeof aceituna)
+
+
+// const primerToppingNaranja = document.querySelector('.topping.fondo-naranja')
+// console.log(primerToppingNaranja);
+// console.log(typeof primerToppingNaranja);
+
+// const primerToppingNaranja = document.querySelector('ul li.fondo-naranja');
+// console.log(primerToppingNaranja)
+// console.log(typeof primerToppingNaranja)
+
+
+const primerToppingNoMarron = document.querySelector('ul li:not(.fondo-marron)');
+console.log(primerToppingNoMarron)
+
