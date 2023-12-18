c435c35cd4a9e9df2322ff9cf98572958cc389bf
Joao Garduño <joao.garduno@gmail.com>
2023-12-18 13:57:00 -0600

    Estructura 9: Texto en el DOM - Modificar el texto

---
+++ 03_textoEnElDom/02_modificarTexto/app.js
@@ -0,0 +1,7 @@
+const titulo = document.getElementById('titulo');
+
+// console.log(titulo)
+// console.log(titulo.innerText)
+// titulo.innerText = 'Mis Toppings Favoritos'
+
+// titulo.textContent = 'Todo sobre...'
