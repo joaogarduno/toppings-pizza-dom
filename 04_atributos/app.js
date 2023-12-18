5270a26c1d5d34918cca6fe935ae19b7f3293ac0
Joao Garduño <joao.garduno@gmail.com>
2023-12-18 14:53:49 -0600

    Estructura 10: Atributos con JavaScript

---
+++ 04_atributos/app.js
@@ -0,0 +1,8 @@
+const enlace = document.getElementsByTagName('a')
+
+// console.log(enlace[0].getAttribute('href'));
+// console.log(enlace[0].removeAttribute('href'));
+
+// Actualizar
+// Asigna un valor a un atributo específico de ese elemento
+console.log(enlace[0].setAttribute('href', 'https://github.com/joaogarduno'));
