425fc173e3e320f7f37d6e198c5d07a7d29aafa9
Joao Garduño <joao.garduno@gmail.com>
2023-12-17 19:53:10 -0600

    Estructura 7: Asignar Estilo con JavaScript - Codigo

--- 02_estilos/app.js
+++ 02_estilos/app.js
@@ -1,1 +1,17 @@
 
+// Estilos
+  // Asignar Estilo con JavaScript
+
+// Cambiar el estilo o alguna propiedad de CSS, cuando el usuario presiona algo, etc.
+const primerTopping = document.querySelector('.topping');
+
+// primerTopping.style.backgroundColor = '#FFF987';
+primerTopping.style.backgroundColor = 'blue';
+primerTopping.style.color = '#6DFF00'
+primerTopping.style.textTransform = 'uppercase'
+
+console.log(primerTopping.style);
+
+// Así es como puedes personalizar el estilo, agregando estilos en linea con JavaScript, ya sabemos como seleccionar los elementos y como personalizar su estilo.
+
+// Existe tambien muchas formas de obtener el "texto" de un elemento
