a1ae0f6cf672c1d5c10494168f82023b11730ab1
Joao Garduño <joao.garduno@gmail.com>
2023-12-20 12:22:59 -0600

    Estructura 12: Clases con JavaScript y el DOM - Agregar una clase

---
+++ 05_clasesJavaScriptDOM/02_agregaClase/app.js
@@ -0,0 +1,7 @@
+const primerTopping = document.querySelector('.topping');
+
+// Metodo para agregar una clase de forma dinamica con el metodo "add"
+// El nombre de la clase debe de ser una cadena de caracteres, y debe estar rodeada por comillas simples y dobles
+primerTopping.classList.add('texto-verde');
+
+console.log(primerTopping.classList);
