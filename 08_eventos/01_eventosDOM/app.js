be080fca7dcd0b0824feb5d00c8592f70a23c4e6
Joao Garduño <joao.garduno@gmail.com>
2023-12-23 10:00:48 -0600

    Estructura 18 - Eventos - Eventos del DOM & Conceptos importantes para Eventos del DOM

---
+++ 08_eventos/01_eventosDOM/app.js
@@ -0,0 +1,27 @@
+// Los eventos son importantes al interactuar con el DOM
+
+// Un evento es algo que ocurre en el sitio web como resultado de la interacción con el usuario o por otras causar como cambios en el estado del dispositivo o cambios en el estado de la ventana, si cambiamos el tamaño de la ventana por alguna razón.
+// Todos esos eventos que ocurran en el sitio web van a desencadenar eventos, eventos formalmente en el sistema es decir en el navegador, eso nos va a permitir saber cuando ocurren esos eventos cuando el usuario esta interactuando con distintos elementos de la página, y tambien nos permite definir como manejar esos eventos en caso de que ocurran, el evento mas común es el evento de "clic"
+// Cuando hacemos uso de un mouse para interactuar con los elementos y hacemos clic sobre algo como por ejemplo un botón un formulario, una imagen, cada uno de estos clic se van a registrar como un evento en el navegador.
+// Y el navegador va a saber sobre cual de esos elemento se hizo clic y vamos a poder decir en caso de que ocurra el clic que es lo que vamos hacer en nuestro código.
+
+// Queremos manejar ese evento, pero, ¿cómo lo vamos a manejar?
+// Todo eso lo podemos definir con JavaScript y también otro evento común es un evento de presionar una tecla en el teclado podemos lograr que ciertos elementos que nos interesan en el DOM, sepan cuando ocurre uno de esos eventos cual de esos elementos fue afectado y deberia reaccionar en base a ese evento y tambien cual de esas teclas fue presionada para saber como manejar a ese evento adecuadamente.
+
+// Por ejemplo, un caso comun seria un juego.
+
+// todo eso es parte del DOM
+// Tambien cambios en el tamaño de la venta, de la pestaña o del navegador, tambien puden ser registrados, es decir, se registran automaticamente y puedes manejarlos adecuadamente si es lo que quieres hacer.
+
+
+// EVENTO - ARRASTRAR
+// Otro evento común en JavaScrip es el evento de arrastrar, cuando en una página web te solicita que arrastres una imagen para subir esa imagen y asignarla como por ejemplo como tu imagen de perfil.
+
+// Ese vento de arrastrar un archivo hacia un elemento en particular y que luego ocurra algo, se maneja gracias a la combinación entre el DOM y JavaScript a través de esos eventos.
+
+
+// EVENTO - swipe
+// También resientemente eventos que deberias conocer y aprender a usarlos adecuadamente pueden ocurrir en dispositivos touch dispositivos tactiles.
+// Puedes manejar distintos gestos que realiza el usuario de diferentes formas, así que podemos ver que los evento son bastante dinamicos y se pueden adaptar a tus necesidades.
+
+// Los eventos mas comunes son los eventos del cursor y del teclado
